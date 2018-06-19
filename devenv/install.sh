b3b490fdaac3de03921c2b801abd25a6acf8e549
Tri Nhan Vu <nhan@necara.de>
2018-06-19 12:14:26 +0200

    create vagrant file, create install script to setup ubuntu for hyperledger fabric

---
+++ devenv/install.sh
@@ -0,0 +1,7 @@
+# Installing prerequisites for running hyperledger fabric for ubuntu
+# source https://hyperledger.github.io/composer/latest/installing/installing-prereqs.html#ubuntu
+curl -O https://hyperledger.github.io/composer/latest/prereqs-ubuntu.sh
+
+chmod u+x prereqs-ubuntu.sh
+
+./prereqs-ubuntu.sh
