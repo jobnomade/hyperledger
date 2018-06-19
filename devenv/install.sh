d823570530c654e5517b823b7832d5074ba72a4b
Tri Nhan Vu <nhan@necara.de>
2018-06-19 12:35:34 +0200

    add comments and bash header

--- devenv/install.sh
+++ devenv/install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Installing prerequisites for running hyperledger fabric for ubuntu
 # source https://hyperledger.github.io/composer/latest/installing/installing-prereqs.html#ubuntu
 curl -O https://hyperledger.github.io/composer/latest/prereqs-ubuntu.sh
